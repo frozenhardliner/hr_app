0f81b75f76ba20d03f143b1a84dc0fa69449d6e9
frozenhardliner <rasilovb@mail.ru>
2024-10-02 10:01:39 +0500

    New update

---
+++ Support.py
@@ -0,0 +1,11 @@
+import streamlit as st
+def app():
+    st.header("Contact Us")
+    st.header("", divider="blue")
+    st.write("For support, please email:")
+    st.write(
+        """
+        - Author: Daulet Yermanov – Daulet.Yermanov@ai-tidaar.com
+        - Or contact us at - info@ai-tidaar.com
+        """
+    )
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import sqlite3
-import Face_Detection, Database, Report
+import Face_Detection, Database, Report, Support
 from streamlit.config import on_config_parsed
 from streamlit_option_menu import option_menu
 st.set_page_config(
@@ -16,11 +16,12 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 def heavy_imports() -> None:
-    from streamlit_option_menu import option_menu
+    from streamlit_option_menu import option_menu    
 on_config_parsed(heavy_imports) 
 conn = sqlite3.connect("worker_database.db")
 class MultiApp:
-
+    with st.sidebar:
+        st.image("img2.png")
     def __init__(self):
         self.apps = []
 
@@ -33,13 +34,20 @@ class MultiApp:
     def run():
         # app = st.sidebar(
         with st.sidebar:        
-            selected = option_menu("Main Menu", ["Face Detection", 'Database', 'Report'], 
-                icons=['house', 'gear'], menu_icon="cast", default_index=0)
-        if selected == "Face Detection":
+            selected = option_menu("Menu", ["Check In/Out", 'Database', 'Report', 'Support'], 
+                icons=['house', 'database-fill-up', 'file-earmark-text', 'person-raised-hand'], menu_icon="cast", default_index=0,
+                    styles={
+        "container": {"padding": "0!important", "background-color": "transparent"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link-selected": {"background-color": "blue"},
+    })
+        if selected == "Check In/Out":
             Face_Detection.app()
         if selected == "Database":
             Database.app()
         if selected == "Report":
             Report.app()
-    
+        if selected == "Support":
+            Support.app()
     run()
+
